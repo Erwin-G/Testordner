afed0133e264b96f1fb2c79006ad84f9af973dcb
unknown <erwin.gutzeit@outlook.de>
2019-09-25 15:52:49 +0200

    .

---
+++ eintest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: eintest
+* Plugin URI: https://www.gutzeit-foto.de
+* Description: Dies ist eiun neues Plugin für Wordpress
+* Author: Erwin Gutzeit
+* Version: 1.0
+* Author URI: https://www.gutzeit-foto.de
+*/
